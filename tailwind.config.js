49d4b1c9fb1568f1d50ff8845eda9d26aab3bc73
Hieu Pham <hieupm2096@gmail.com>
2022-01-11 15:22:40 +0700

    add: finish challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "#8BACD9",
+        cyan: "#00FFF8",
+        "main-background": "#0D192C",
+        "card-background": "#15263F",
+        line: "#2E405A",
+      },
+    },
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
